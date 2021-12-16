5f29bfaf53528c8aaa08cd64923871c818067df9
Radan Ganchev <radan.ganchev@gmail.com>
2021-12-16 10:40:54 +0200

    Use a Heap for storing distances in the Dijkstra algorithm

--- solutions/15-chiton.mjs
+++ solutions/15-chiton.mjs
@@ -1,14 +1,84 @@
 import { fetchInputForDay } from '../input.mjs'
 import _ from 'lodash'
 
+class Heap {
+  constructor(elements) {
+    this.elements = new Array()
+    this.indexMap = new Map()
+    elements.forEach(element => this.add(element))
+  }
+
+  takeMin() {
+    this._swap(0, this.elements.length - 1)
+    const min = this.elements.pop()
+    this.indexMap.delete(min.id)
+    this._sink(0)
+    return min
+  }
+
+  has(id) {
+    return this.indexMap.has(id)
+  }
+
+  getWeight(id) {
+    return this.elements[this.indexMap.get(id)].weight
+  }
+
+  add({ id, weight }) {
+    this.elements.push({ id, weight })
+    this.indexMap.set(id, this.elements.length - 1)
+    this._float(this.elements.length - 1)
+  }
+
+  update({ id, weight }) {
+    const index = this.indexMap.get(id)
+    this.elements[index].weight = weight
+    if (!this._float(index)) this._sink(index)
+  }
+
+  _float(index) {
+    let current = index
+    while (current > 0 && this.elements[current].weight < this.elements[Math.floor(current / 2)].weight) {
+      const parent = Math.floor(current / 2)
+      this._swap(current, parent)
+      current = parent
+    }
+    return current !== index
+  }
+
+  _sink(index) {
+    let current = index
+    while (2 * current < this.elements.length) {
+      const children = [2 * current]
+      if (2 * current + 1 < this.elements.length) children.push(2 * current + 1)
+      const smallestChild = _.minBy(children, i => this.elements[i].weight)
+
+      if (this.elements[current].weight > this.elements[smallestChild].weight) {
+        this._swap(current, smallestChild)
+        current = smallestChild
+      } else {
+        break
+      }
+    }
+    return current !== index
+  }
+
+  _swap(i, j) {
+    const buffer = this.elements[i]
+    this.elements[i] = this.elements[j]
+    this.elements[j] = buffer
+    this.indexMap.set(this.elements[i].id, i)
+    this.indexMap.set(this.elements[j].id, j)
+  }
+}
+
 export async function solve() {
   const input = _.compact((await fetchInputForDay(15)).split('\n')).map(line => line.split('').map(Number))
 
   function dijkstra(input) {
-    const graph = new Uint32Array(input.flatMap(line => line))
+    const graph = input.flatMap(line => line)
     const size = input.length
-    const minDistances = new Uint32Array(graph.length).fill(1 << 31)
-    const unvisited = new Set(_.times(graph.length - 1, i => i + 1))
+    const distances = new Heap(graph.map((_, i) => ({ id: i, weight: Infinity })))
   
     function adj(x) {
       const adjacent = []
@@ -19,21 +89,20 @@ export async function solve() {
       return adjacent
     }
   
-    let current = 0
-    minDistances[0] = 0
-    while (current !== graph.length - 1) {
-      adj(current).forEach(i => minDistances[i] = Math.min(minDistances[i], minDistances[current] + graph[i]))
-      unvisited.delete(current)
-      current = _.minBy([...unvisited], i => minDistances[i])
-    }
+    distances.update({ id: 0, weight: 0 })
+    let current
+    do {
+      current = distances.takeMin()
+      adj(current.id)
+        .filter(i => distances.has(i))
+        .forEach(i => distances.update({ id: i, weight: Math.min(distances.getWeight(i), current.weight + graph[i]) }))
+    } while (current.id !== graph.length - 1)
 
-    return minDistances[current]
+    return current.weight
   }
 
   console.log('Answer, part 1:', dijkstra(input))
   
   const tiledInput = _.times(5, i => input.map(line => _.times(5, j => line.map(x => (x - 1 + i + j) % 9 + 1)).flatMap(line => line))).flatMap(tile => tile)
-  // Need a more efficient implementation. Dijkstra is very slow on the larger input, but still
-  // manages to complete in ~10 minutes.
   console.log('Answer, part 2:', dijkstra(tiledInput))
 }
